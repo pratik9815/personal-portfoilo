65e03aed278a79c2f4d63cbcb592629c8a9f91c7
pratik9815 <pchamlagain12@gmail.com>
2024-05-20 20:24:09 +0545

    making multiple sections

--- script.js
+++ script.js
@@ -18,8 +18,6 @@ function isChecked()
 
 document.addEventListener("DOMContentLoaded",function() {
     var navLinks = document.querySelectorAll('.nav-item');
-    // navLinks.forEach
-    console.log(navLinks.length);
     navLinks.forEach(function(links){
         links.addEventListener('click',function(){
             document.getElementById('checkbox').checked = false;
@@ -35,11 +33,11 @@ function reveal(){
     var reveals = document.querySelectorAll('.reveal');
 
     for(var i = 0; i < reveals.length; i++){
-        var windowheight = window.innerHeight;
+        var windowheight = window.innerHeight ;
         var revealtop = reveals[i].getBoundingClientRect().top;
         var revealpoint = 150;
         
-        if(revealtop < windowheight - revealpoint){
+        if(revealtop < windowheight - revealpoint ){
             reveals[i].classList.add('active');
         }
         else{
